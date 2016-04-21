d10619ad4107d65dee44c3ee06d853ab53301c79
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-04-21 13:52:50 +0200

    overload construct for roleService

--- src/MonarcCore/Service/RoleService.php
+++ src/MonarcCore/Service/RoleService.php
@@ -1,19 +1,28 @@
 <?php
 namespace MonarcCore\Service;
 
-use MonarcCore\Model\Entity\Role;
-
 class RoleService extends AbstractService
 {
 
     protected $roleEntity;
     protected $config;
 
+    public function __construct($serviceFactory = null)
+    {
+        if (is_array($serviceFactory)){
+            foreach($serviceFactory as $k => $v){
+                $this->set($k,$v);
+            }
+        } else {
+            $this->serviceFactory = $serviceFactory;
+        }
+    }
+
     public function getFilteredCount() {
 
         $config = $this->config;
         $roles = $config['roles'];
-        
+
         return count($roles);
     }
 
