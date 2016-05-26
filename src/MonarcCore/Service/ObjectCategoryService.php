41df8a3b2d66ad8521a195e18b6604203ef965fa
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-05-26 16:49:24 +0200

    service objects categories

--- src/MonarcCore/Service/ObjectCategoryService.php
+++ src/MonarcCore/Service/ObjectCategoryService.php
@@ -22,19 +22,95 @@ class ObjectCategoryService extends AbstractService
         $entity = $this->get('entity');
         $entity->exchangeArray($data);
 
-        //parent
+        //parent and root
         $parentValue = $entity->get('parent');
         if (!empty($parentValue)) {
             $parentEntity = $this->get('table')->getEntity($parentValue);
             $entity->setParent($parentEntity);
+
+            $rootEntity = $this->getRoot($entity);
+            $entity->setRoot($rootEntity);
+        } else {
+            $entity->setParent(null);
+            $entity->setRoot(null);
         }
-
-        //root
-
-
-
 
         return $this->get('table')->save($entity);
 
+    }
+
+    /**
+     * Update
+     *
+     * @param $id
+     * @param $data
+     * @return mixed
+     */
+    public function update($id, $data){
+
+        $entity = $this->get('table')->getEntity($id);
+        $entity->exchangeArray($data);
+
+        //parent and root
+        $parentValue = $entity->get('parent');
+        if (!empty($parentValue)) {
+            $parentEntity = $this->get('table')->getEntity($parentValue);
+            $entity->setParent($parentEntity);
+
+            $rootEntity = $this->getRoot($entity);
+            $entity->setRoot($rootEntity);
+        } else {
+            $entity->setParent(null);
+            $entity->setRoot(null);
+        }
+
+        if ((! array_key_exists('parent', $data)) || (is_null($data['parent']))) {
+            $entity->setParent(null);
+            $entity->setRoot(null);
+        }
+
+        return $this->get('table')->save($entity);
+    }
+
+    /**
+     * Delete
+     *
+     * @param $id
+     */
+    public function delete($id) {
+
+        $this->get('table')->getRepository()->createQueryBuilder('t')
+            ->update()
+            ->set('t.parent', ':parentset')
+            ->where('t.parent = :parentwhere')
+            ->setParameter(':parentset', null)
+            ->setParameter(':parentwhere', $id)
+            ->getQuery()
+            ->getResult();
+
+        $this->get('table')->getRepository()->createQueryBuilder('t')
+            ->update()
+            ->set('t.root', ':rootset')
+            ->where('t.root = :rootwhere')
+            ->setParameter(':rootset', null)
+            ->setParameter(':rootwhere', $id)
+            ->getQuery()
+            ->getResult();
+
+        $this->get('table')->delete($id);
+    }
+
+    /**
+     * Get root
+     *
+     * @param $entity
+     * @return mixed
+     */
+    public function getRoot($entity) {
+        if (!is_null($entity->getParent())) {
+            return $this->getRoot($entity->getParent());
+        } else {
+            return $entity;
+        }
     }
 }
