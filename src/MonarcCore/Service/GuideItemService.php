3b0d4504c17503fcf763ed89235994ef805650e3
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-06-13 16:17:11 +0200

    add guide item service

---
+++ src/MonarcCore/Service/GuideItemService.php
@@ -0,0 +1,16 @@
+<?php
+namespace MonarcCore\Service;
+
+/**
+ * Guide Item Service
+ *
+ * Class GuideItemService
+ * @package MonarcCore\Service
+ */
+class GuideItemService extends AbstractService
+{
+    protected $guideTable;
+
+    protected $dependencies = ['guide'];
+
+}
