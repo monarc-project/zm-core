b648f9a6fa40b3c88b80ee132b35b181b4686503
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-07-05 14:42:37 +0200

    fix bug filter mesaures

--- src/MonarcCore/Service/MeasureService.php
+++ src/MonarcCore/Service/MeasureService.php
@@ -10,7 +10,6 @@ namespace MonarcCore\Service;
 class MeasureService extends AbstractService
 {
     protected $filterColumns = array(
-        'label1', 'label2', 'label3', 'label4',
         'description1', 'description2', 'description3', 'description4',
         'code', 'status'
     );
