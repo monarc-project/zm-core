80473f0ecacfbf111a8869596cca0324bae3ceb8
Guillaume Lesniak <guillaume@netlor.fr>
2016-08-23 10:38:43 +0200

    Scales: Revamp backend

--- src/MonarcCore/Service/ScaleCommentService.php
+++ src/MonarcCore/Service/ScaleCommentService.php
@@ -16,11 +16,6 @@ class ScaleCommentService extends AbstractService
     protected $dependencies = ['scale', 'scaleTypeImpact'];
 
     public function getList($page = 1, $limit = 25, $order = null, $filter = null, $filterAnd = null){
-
-        $scale = $this->get('scaleService')->getByAnrAndType($filterAnd['anr'], $filterAnd['type']);
-
-        $filterAnd = ['scale' => $scale['id']];
-
         $comments = $this->get('table')->fetchAllFiltered(
             array_keys($this->get('entity')->getJsonArray()),
             $page,
@@ -30,12 +25,12 @@ class ScaleCommentService extends AbstractService
             $filterAnd
         );
 
-        $scaleTypes = $this->get('scaleService')->getTypes();
+        /*$scaleTypes = $this->get('scaleService')->getTypes();
         $scaleTypesImpact = $this->get('scaleTypeService')->getTypes();
 
         foreach ($comments as $key => $comment) {
             $comments[$key]['scaleTypeImpact']->type = $scaleTypesImpact[$comment['scaleTypeImpact']->type];
-        }
+        }*/
 
         return $comments;
     }
