a9c7bc0a210a252842439b9457634451c3b0b2c3
Thomas Metois <thomas@netlor.fr>
2016-09-05 17:54:30 +0200

    Filter AMVs & query

--- src/MonarcCore/Service/AmvService.php
+++ src/MonarcCore/Service/AmvService.php
@@ -27,6 +27,166 @@ class AmvService extends AbstractService
     protected $dependencies = ['asset', 'threat', 'vulnerability', 'measure1', 'measure2', 'measure3'];
 
     /**
+     * Get List
+     *
+     * @param int $page
+     * @param int $limit
+     * @param null $order
+     * @param null $filter
+     * @return mixed
+     */
+    public function getList($page = 1, $limit = 25, $order = null, $filter = null, $filterAnd = null){
+        $filterJoin = array(
+            array(
+                'as' => 'a',
+                'rel' => 'asset',
+            ),
+            array(
+                'as' => 'th',
+                'rel' => 'threat',
+            ),
+            array(
+                'as' => 'v',
+                'rel' => 'vulnerability',
+            ),
+        );
+        $filterLeft = array(
+            array(
+                'as' => 'm1',
+                'rel' => 'measure1',
+            ),
+            array(
+                'as' => 'm2',
+                'rel' => 'measure2',
+            ),
+            array(
+                'as' => 'm3',
+                'rel' => 'measure3',
+            ),
+        );
+        $filtersCol = array();
+        $filtersCol[] = 'a.code';
+        $filtersCol[] = 'a.label1';
+        $filtersCol[] = 'a.label2';
+        $filtersCol[] = 'a.label3';
+        $filtersCol[] = 'a.description1';
+        $filtersCol[] = 'a.description2';
+        $filtersCol[] = 'a.description3';
+        $filtersCol[] = 'th.code';
+        $filtersCol[] = 'th.label1';
+        $filtersCol[] = 'th.label2';
+        $filtersCol[] = 'th.label3';
+        $filtersCol[] = 'th.description1';
+        $filtersCol[] = 'th.description2';
+        $filtersCol[] = 'th.description3';
+        $filtersCol[] = 'v.code';
+        $filtersCol[] = 'v.label1';
+        $filtersCol[] = 'v.label2';
+        $filtersCol[] = 'v.label3';
+        $filtersCol[] = 'v.description1';
+        $filtersCol[] = 'v.description2';
+        $filtersCol[] = 'v.description3';
+        $filtersCol[] = 'm1.code';
+        $filtersCol[] = 'm1.description1';
+        $filtersCol[] = 'm1.description2';
+        $filtersCol[] = 'm1.description3';
+        $filtersCol[] = 'm2.code';
+        $filtersCol[] = 'm2.description1';
+        $filtersCol[] = 'm2.description2';
+        $filtersCol[] = 'm2.description3';
+        $filtersCol[] = 'm3.code';
+        $filtersCol[] = 'm3.description1';
+        $filtersCol[] = 'm3.description2';
+        $filtersCol[] = 'm3.description3';
+
+        return $this->get('table')->fetchAllFiltered(
+            array_keys($this->get('entity')->getJsonArray()),
+            $page,
+            $limit,
+            $this->parseFrontendOrder($order),
+            $this->parseFrontendFilter($filter, $filtersCol),
+            $filterAnd,
+            $filterJoin,
+            $filterLeft
+        );
+    }
+
+    public function getFilteredCount($page = 1, $limit = 25, $order = null, $filter = null, $filterAnd = null){
+        $filterJoin = array(
+            array(
+                'as' => 'a',
+                'rel' => 'asset',
+            ),
+            array(
+                'as' => 'th',
+                'rel' => 'threat',
+            ),
+            array(
+                'as' => 'v',
+                'rel' => 'vulnerability',
+            ),
+        );
+        $filterLeft = array(
+            array(
+                'as' => 'm1',
+                'rel' => 'measure1',
+            ),
+            array(
+                'as' => 'm2',
+                'rel' => 'measure2',
+            ),
+            array(
+                'as' => 'm3',
+                'rel' => 'measure3',
+            ),
+        );
+        $filtersCol = array();
+        $filtersCol[] = 'a.code';
+        $filtersCol[] = 'a.label1';
+        $filtersCol[] = 'a.label2';
+        $filtersCol[] = 'a.label3';
+        $filtersCol[] = 'a.description1';
+        $filtersCol[] = 'a.description2';
+        $filtersCol[] = 'a.description3';
+        $filtersCol[] = 'th.code';
+        $filtersCol[] = 'th.label1';
+        $filtersCol[] = 'th.label2';
+        $filtersCol[] = 'th.label3';
+        $filtersCol[] = 'th.description1';
+        $filtersCol[] = 'th.description2';
+        $filtersCol[] = 'th.description3';
+        $filtersCol[] = 'v.code';
+        $filtersCol[] = 'v.label1';
+        $filtersCol[] = 'v.label2';
+        $filtersCol[] = 'v.label3';
+        $filtersCol[] = 'v.description1';
+        $filtersCol[] = 'v.description2';
+        $filtersCol[] = 'v.description3';
+        $filtersCol[] = 'm1.code';
+        $filtersCol[] = 'm1.description1';
+        $filtersCol[] = 'm1.description2';
+        $filtersCol[] = 'm1.description3';
+        $filtersCol[] = 'm2.code';
+        $filtersCol[] = 'm2.description1';
+        $filtersCol[] = 'm2.description2';
+        $filtersCol[] = 'm2.description3';
+        $filtersCol[] = 'm3.code';
+        $filtersCol[] = 'm3.description1';
+        $filtersCol[] = 'm3.description2';
+        $filtersCol[] = 'm3.description3';
+
+        return $this->get('table')->countFiltered(
+            $page,
+            $limit,
+            $this->parseFrontendOrder($order),
+            $this->parseFrontendFilter($filter, $filtersCol),
+            $filterAnd,
+            $filterJoin,
+            $filterLeft
+        );
+    }
+
+    /**
      * Create
      *
      * @param $data
