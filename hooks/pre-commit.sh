c5af3c40b06ed1b0cc2cab3cec709aaf3329685b
Guillaume Lesniak <guillaume@netlor.fr>
2016-09-08 10:16:42 +0200

    hooks: Add pre-commit hook

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+### Don't allow commiting bad stuff
+
+git diff --cached --diff-filter=ACMR
+
+VAR=$(git diff --cached --diff-filter=ACMR | grep -w "var_dump")
+if [ ! -z "$VAR" ]; then
+	echo "You've left a var_dump in one of your files! You don't want to commit that..."
+	exit 1
+fi
+
+exit 0
