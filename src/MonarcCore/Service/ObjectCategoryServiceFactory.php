ee734d5bb9d6a9bb154c91a82f94d2053096648d
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-09-09 10:34:41 +0200

    fix bug objects categories

--- src/MonarcCore/Service/ObjectCategoryServiceFactory.php
+++ src/MonarcCore/Service/ObjectCategoryServiceFactory.php
@@ -6,7 +6,6 @@ class ObjectCategoryServiceFactory extends AbstractServiceFactory
     protected $ressources = array(
         'table'=> '\MonarcCore\Model\Table\ObjectCategoryTable',
         'entity'=> '\MonarcCore\Model\Entity\ObjectCategory',
-        'anrTable'=> '\MonarcCore\Model\Table\AnrTable',
     );
 
 }
