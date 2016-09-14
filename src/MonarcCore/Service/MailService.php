ba6d95ac4695ba6357888f92345147b7ea4bdac0
Benjamin Ganivet <benjamin@netlor.fr>
2016-09-14 17:25:19 +0200

    Forgotten password mail : Fix header To twice

--- src/MonarcCore/Service/MailService.php
+++ src/MonarcCore/Service/MailService.php
@@ -22,7 +22,6 @@ class MailService extends AbstractService
         $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
         $headers .= 'Reply-To: Cases <info@cases.lu>' . "\r\n";
-        $headers .= 'To: ' . $email . ' <' . $email .' >' . "\r\n";
         $headers .= 'From: Cases <info@cases.lu>' . "\r\n";
 
         mail($email, $subject, $message, $headers);
