99c6bec1115fbcc01fcc376a7ba7c4381ee40f0e
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-10-05 15:10:07 +0200

    delete table interviews for bo

---
+++ migrations/db/20161005150617_delete_interviews.php
@@ -0,0 +1,56 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class DeleteInterviews extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up()
+    {
+        $this->dropTable('interviews');
+    }
+
+    public function down()
+    {
+        // Migration for table interviews
+        $table = $this->table('interviews');
+        $table
+            ->addColumn('anr_id', 'integer', array('null' => true, 'signed' => false))
+            ->addColumn('date', 'datetime', array('null' => true))
+            ->addColumn('service', 'text', array('null' => true))
+            ->addColumn('content', 'text', array('null' => true, 'limit' => MysqlAdapter::TEXT_LONG))
+            ->addColumn('creator', 'string', array('null' => true, 'limit' => 255))
+            ->addColumn('created_at', 'datetime', array('null' => true))
+            ->addColumn('updater', 'string', array('null' => true, 'limit' => 255))
+            ->addColumn('updated_at', 'datetime', array('null' => true))
+            ->addIndex(array('anr_id'))
+            ->create();
+        $table->changeColumn('id', 'integer',array('identity'=>true,'signed'=>false))->update();
+        
+        $table = $this->table('interviews');
+        $table
+            ->addForeignKey('anr_id', 'anrs', 'id', array('delete' => 'CASCADE','update' => 'RESTRICT'))
+            ->update();
+    }
+}
