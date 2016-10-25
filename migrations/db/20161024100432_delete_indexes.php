96ae0658f3823d99dd80bcf2265bcf30d633534f
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-10-25 13:23:44 +0200

    fix bug migration

--- migrations/db/20161024100432_delete_indexes.php
+++ migrations/db/20161024100432_delete_indexes.php
@@ -27,9 +27,9 @@ class DeleteIndexes extends AbstractMigration
      */
     public function change()
     {
-        $this->query('ALTER TABLE monarc_cli.rolf_categories DROP FOREIGN KEY rolf_categories_ibfk_1');
-        $this->query('ALTER TABLE monarc_cli.rolf_risks DROP FOREIGN KEY rolf_risks_ibfk_1');
-        $this->query('ALTER TABLE monarc_cli.rolf_tags DROP FOREIGN KEY rolf_tags_ibfk_1');
+        $this->query('ALTER TABLE monarc_common.rolf_categories DROP FOREIGN KEY rolf_categories_ibfk_1');
+        $this->query('ALTER TABLE monarc_common.rolf_risks DROP FOREIGN KEY rolf_risks_ibfk_1');
+        $this->query('ALTER TABLE monarc_common.rolf_tags DROP FOREIGN KEY rolf_tags_ibfk_1');
 
         //rolf categories
         $table = $this->table('rolf_categories');
