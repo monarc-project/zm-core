74131d82d764e41764574a21b290de5325a3379e
Thomas Metois <thomas@netlor.fr>
2016-11-22 17:04:01 +0100

    Use Doctrine SuperClass

---
+++ src/MonarcCore/Model/Entity/MeasureSuperClass.php
@@ -0,0 +1,200 @@
+<?php
+
+namespace MonarcCore\Model\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Measure
+ *
+ * @ORM\Table(name="measures", indexes={
+ *      @ORM\Index(name="anr", columns={"anr_id"})
+ * })
+ * @ORM\MappedSuperclass
+ */
+class MeasureSuperClass extends AbstractEntity
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var \MonarcCore\Model\Entity\Anr
+     *
+     * @ORM\ManyToOne(targetEntity="MonarcCore\Model\Entity\AnrSuperClass", cascade={"persist"})
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="anr_id", referencedColumnName="id", nullable=true)
+     * })
+     */
+    protected $anr;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=255, nullable=true)
+     */
+    protected $code;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description1", type="string", length=255, nullable=true)
+     */
+    protected $description1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description2", type="string", length=255, nullable=true)
+     */
+    protected $description2;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description3", type="string", length=255, nullable=true)
+     */
+    protected $description3;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description4", type="string", length=255, nullable=true)
+     */
+    protected $description4;
+
+    /**
+     * @var smallint
+     *
+     * @ORM\Column(name="status", type="smallint", options={"unsigned":true, "default":1})
+     */
+    protected $status = '1';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="creator", type="string", length=255, nullable=true)
+     */
+    protected $creator;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     */
+    protected $createdAt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="updater", type="string", length=255, nullable=true)
+     */
+    protected $updater;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     */
+    protected $updatedAt;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Model
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAnr()
+    {
+        return $this->anr;
+    }
+
+    /**
+     * @param int $anr
+     * @return Measure
+     */
+    public function setAnr($anr)
+    {
+        $this->anr = $anr;
+        return $this;
+    }
+
+    public function getInputFilter($partial = false){
+
+        if (!$this->inputFilter) {
+            parent::getInputFilter($partial);
+
+            $texts = ['description1', 'description2', 'description3', 'description4'];
+
+            foreach($texts as $text) {
+                $this->inputFilter->add(array(
+                    'name' => $text,
+                    'required' => ((strchr($text, (string) $this->getLanguage())) && (!$partial)) ? true : false,
+                    'allow_empty' => false,
+                    'filters' => array(),
+                    'validators' => array(),
+                ));
+            }
+            $validatorsCode = [];
+            if (!$partial) {
+                $validatorsCode = array(
+                    array(
+                        'name' => '\MonarcCore\Validator\UniqueCode',
+                        'options' => array(
+                            'entity' => $this
+                        ),
+                    ),
+                );
+            }
+
+            $this->inputFilter->add(array(
+                'name' => 'code',
+                'required' => ($partial) ? false : true,
+                'allow_empty' => false,
+                'filters' => array(),
+                'validators' => $validatorsCode
+            ));
+
+            $this->inputFilter->add(array(
+                'name' => 'status',
+                'required' => false,
+                'allow_empty' => false,
+                'filters' => array(
+                    array('name' => 'ToInt'),
+                ),
+                'validators' => array(
+                    array(
+                        'name' => 'InArray',
+                        'options' => array(
+                            'haystack' => array(self::STATUS_INACTIVE, self::STATUS_ACTIVE),
+                        ),
+                    ),
+                ),
+            ));
+        }
+
+
+        return $this->inputFilter;
+    }
+}
+
