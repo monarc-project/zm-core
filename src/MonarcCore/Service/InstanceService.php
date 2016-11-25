b7beb7b6f5c8b3071d3a026f2fd53bd39549bd6c
Jerome De Almeida <dealmeida.jerome@gmail.com>
2016-11-25 14:17:31 +0100

    fix bug parent instance

--- src/MonarcCore/Service/InstanceService.php
+++ src/MonarcCore/Service/InstanceService.php
@@ -90,7 +90,7 @@ class InstanceService extends AbstractService
         //set impacts
         /** @var InstanceTable $table */
         //$table = $this->get('table');
-        //$parent = ($data['parent']) ? $table->getEntity($data['parent']) : null;
+        $parent = ($data['parent']) ? $table->getEntity($data['parent']) : null;
         //$parentId = ($data['parent']) ? ($data['parent']) : null;
 
         $this->updateImpactsInherited($anrId, $parent, $data);
