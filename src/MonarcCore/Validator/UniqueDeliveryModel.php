3f59d075070e484a4b0a0234819129cd9c45d2e2
Thomas Metois <thomas@netlor.fr>
2017-02-22 17:48:29 +0100

    EVO > use white-spaces instead of tabs

--- src/MonarcCore/Validator/UniqueDeliveryModel.php
+++ src/MonarcCore/Validator/UniqueDeliveryModel.php
@@ -6,31 +6,31 @@ use Zend\Validator\AbstractValidator;
 
 class UniqueDeliveryModel extends AbstractValidator
 {
-	protected $options = array(
-		'adapter' => null,
-		'category' => 0,
-		'id' => 0,
-	);
+    protected $options = array(
+        'adapter' => null,
+        'category' => 0,
+        'id' => 0,
+    );
 
-	const ALREADYUSED = "ALREADYUSED";
+    const ALREADYUSED = "ALREADYUSED";
 
-	protected $messageTemplates = array(
-		self::ALREADYUSED => 'This category is already used',
-	);
-	public function __construct(array $options = array()){
+    protected $messageTemplates = array(
+        self::ALREADYUSED => 'This category is already used',
+    );
+    public function __construct(array $options = array()){
        parent::__construct($options);
     }
 
-	public function isValid($value){
-		if(empty($this->options['adapter'])){
-			return false;
-		}else{
-			$res = $this->options['adapter']->getRepository('\MonarcCore\Model\Entity\DeliveriesModels')->findOneByCategory($value);
-			if(!empty($res) && $this->options['id'] != $res->get('id')){
-				$this->error(self::ALREADYUSED);
-				return false;
-			}
-		}
-		return true;
-	}
+    public function isValid($value){
+        if(empty($this->options['adapter'])){
+            return false;
+        }else{
+            $res = $this->options['adapter']->getRepository('\MonarcCore\Model\Entity\DeliveriesModels')->findOneByCategory($value);
+            if(!empty($res) && $this->options['id'] != $res->get('id')){
+                $this->error(self::ALREADYUSED);
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/MonarcCore/Validator/UniqueEmail.php
+++ src/MonarcCore/Validator/UniqueEmail.php
@@ -6,31 +6,31 @@ use Zend\Validator\AbstractValidator;
 
 class UniqueEmail extends AbstractValidator
 {
-	protected $options = array(
-		'adapter' => null,
-		'id' => 0,
-	);
+    protected $options = array(
+        'adapter' => null,
+        'id' => 0,
+    );
 
-	const ALREADYUSED = "ALREADYUSED";
+    const ALREADYUSED = "ALREADYUSED";
 
-	protected $messageTemplates = array(
-		self::ALREADYUSED => 'This email is already used',
-	);
-	public function __construct(array $options = array()){
+    protected $messageTemplates = array(
+        self::ALREADYUSED => 'This email is already used',
+    );
+    public function __construct(array $options = array()){
        parent::__construct($options);
     }
 
-	public function isValid($value){
+    public function isValid($value){
 
-		if(empty($this->options['adapter'])){
-			return false;
-		}else{
-			$res = $this->options['adapter']->getRepository('\MonarcCore\Model\Entity\User')->findOneByEmail($value);
-			if(!empty($res) && $this->options['id'] != $res->get('id')){
-				$this->error(self::ALREADYUSED);
-				return false;
-			}
-		}
-		return true;
-	}
+        if(empty($this->options['adapter'])){
+            return false;
+        }else{
+            $res = $this->options['adapter']->getRepository('\MonarcCore\Model\Entity\User')->findOneByEmail($value);
+            if(!empty($res) && $this->options['id'] != $res->get('id')){
+                $this->error(self::ALREADYUSED);
+                return false;
+            }
+        }
+        return true;
+    }
 }
