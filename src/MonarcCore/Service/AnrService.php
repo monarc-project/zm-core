3c89d0fa56c998651a68516ae93b7e7550fc9c3a
Thomas Metois <thomas@netlor.fr>
2017-02-22 16:30:28 +0100

    EVO: Anr Service > remove duplicate build scales data

--- src/MonarcCore/Service/AnrService.php
+++ src/MonarcCore/Service/AnrService.php
@@ -54,26 +54,15 @@ class AnrService extends AbstractService
                 $data['scales'][$i]['min'] = 0;
             }
         }
-        $scales = [
-            [
+        $scales = [];
+        for($i = 1; $i <= 3; $i++){
+            $scales[] = [
                 'anr' => $anrId,
-                'type' => 1,
-                'min' => $data['scales'][1]['min'],
-                'max' => (isset($data['scales'][1]['max']) ? $data['scales'][1]['max'] : 3),
-            ],
-            [
-                'anr' => $anrId,
-                'type' => 2,
-                'min' => $data['scales'][2]['min'],
-                'max' => (isset($data['scales'][2]['max']) ? $data['scales'][2]['max'] : 4),
-            ],
-            [
-                'anr' => $anrId,
-                'type' => 3,
-                'min' => $data['scales'][3]['min'],
-                'max' => (isset($data['scales'][3]['max']) ? $data['scales'][3]['max'] : 3),
-            ],
-        ];
+                'type' => $i,
+                'min' => $data['scales'][$i]['min'],
+                'max' => (isset($data['scales'][$i]['max']) ? $data['scales'][$i]['max'] : ($i == 2?4:3)),
+            ];
+        }
         $i = 1;
         $nbScales = count($scales);
         foreach ($scales as $scale) {
