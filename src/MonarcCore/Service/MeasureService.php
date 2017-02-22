e419212771d80e8a05a49f52fb0cb96b6e89320a
Thomas Metois <thomas@netlor.fr>
2017-02-22 14:22:32 +0100

    MeasureService > remove duplicate create fct

--- src/MonarcCore/Service/MeasureService.php
+++ src/MonarcCore/Service/MeasureService.php
@@ -19,38 +19,6 @@ class MeasureService extends AbstractService
     protected $forbiddenFields = ['anr'];
 
     /**
-     * Create
-     *
-     * @param $data
-     * @param bool $last
-     * @return mixed
-     * @throws \Exception
-     */
-    public function create($data, $last = true)
-    {
-        $class = $this->get('entity');
-        $entity = new $class();
-        $entity->setLanguage($this->getLanguage());
-        $entity->setDbAdapter($this->get('table')->getDb());
-        if (isset($data['anr']) && strlen($data['anr'])) {
-            /** @var AnrTable $anrTable */
-            $anrTable = $this->get('anrTable');
-            $anr = $anrTable->getEntity($data['anr']);
-
-            if (!$anr) {
-                throw new \Exception('Risk analysis does not exist', 412);
-            }
-            $entity->setAnr($anr);
-        }
-        $entity->exchangeArray($data);
-
-        $dependencies = (property_exists($this, 'dependencies')) ? $this->dependencies : [];
-        $this->setDependencies($entity, $dependencies);
-
-        return $this->get('table')->save($entity);
-    }
-
-    /**
      * Patch
      *
      * @param $id
@@ -85,6 +53,7 @@ class MeasureService extends AbstractService
             $filterAnd
         );
 
+        // TODO: try to order in SQL instead of php with usort
         if ($order == "code" || $order == "-code") {
             $desc = ($order == "-code");
 
