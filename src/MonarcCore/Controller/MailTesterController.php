3ad42425d06d860b30e868a003a2d15d005217df
Thomas Metois <thomas@netlor.fr>
2017-04-21 15:59:23 +0200

    EVO: add cli route for testing email send

---
+++ src/MonarcCore/Controller/MailTesterController.php
@@ -0,0 +1,30 @@
+<?php
+namespace MonarcCore\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Console\Request as ConsoleRequest;
+
+use Zend\Console\Exception\RuntimeException as ConsoleException;
+
+use \MonarcCore\Service\MailTesterService;
+
+class MailTesterController extends AbstractActionController
+{
+    protected $MailTesterService;
+
+    public function __construct(MailTesterService $MailTesterService){
+        $this->MailTesterService = $MailTesterService;
+    }
+
+    public function indexAction(){
+        $request = $this->getRequest();
+        if (!$request instanceof ConsoleRequest) {
+            throw new RuntimeException('You can only use this action from a console!');
+        }
+        try {
+            $this->MailTesterService->send($request);
+        } catch (ConsoleException $e) {
+            // Could not get console adapter - most likely we are not running inside a console window.
+        }
+    }
+}
