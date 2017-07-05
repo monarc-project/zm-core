2bdbfb335c13e0931190778965eb5c1dfe93d9c7
Cédric Bonhomme <cedric@cedricbonhomme.org>
2017-07-05 14:01:19 +0200

    Updated links to the GitHub organization.

--- src/MonarcCore/Service/Model/Entity/RolfTagServiceModelEntity.php
+++ src/MonarcCore/Service/Model/Entity/RolfTagServiceModelEntity.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @link      https://github.com/CASES-LU for the canonical source repository
+ * @link      https://github.com/monarc-project for the canonical source repository
  * @copyright Copyright (c) Cases is a registered trademark of SECURITYMADEIN.LU
  * @license   MyCases is licensed under the GNU Affero GPL v3 - See license.txt for more information
  */
