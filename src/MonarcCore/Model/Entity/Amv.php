1c1959f99a098cd906fc0196add2d573caebddf7
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-03-20 15:15:35 +0100

    the filter are not now the same between fo and bo for amvs

--- src/MonarcCore/Model/Entity/Amv.php
+++ src/MonarcCore/Model/Entity/Amv.php
@@ -22,4 +22,80 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Amv extends AmvSuperclass
 {
+  public function getInputFilter($partial = false)
+  {
+      if (!$this->inputFilter) {
+          parent::getInputFilter($partial);
+
+          $texts = ['vulnerability', 'asset'];
+
+          foreach ($texts as $text) {
+              $this->inputFilter->add(array(
+                  'name' => $text,
+                  'required' => ($partial) ? false : true,
+                  'allow_empty' => false,
+                  'filters' => array(
+                      array(
+                          'name' => 'Digits',
+                      ),
+                  ),
+                  'validators' => array(),
+              ));
+          }
+
+          $this->inputFilter->add(array(
+              'name' => 'threat',
+              'required' => ($partial) ? false : true,
+              'allow_empty' => false,
+              'filters' => array(
+                  array(
+                      'name' => 'Digits',
+                  ),
+              ),
+              'validators' => array(
+                  array(
+                      'name' => 'Callback',//'\MonarcCore\Validator\UniqueAMV',
+                      'options' => array(
+                          'messages' => array(
+                              \Zend\Validator\Callback::INVALID_VALUE => 'This AMV link is already used',
+                          ),
+                          'callback' => function ($value, $context = array()) use ($partial) {
+                              if (!$partial) {
+                                  $adapter = $this->getDbAdapter();
+                                  if (empty($adapter)) {
+                                      return false;
+                                  } else {
+                                      $res = $adapter->getRepository(get_class($this))->createQueryBuilder('a')
+                                          ->select(array('a.id'))
+                                          ->where(' a.vulnerability = :vulnerability ')
+                                          ->andWhere(' a.asset = :asset ')
+                                          ->andWhere(' a.threat = :threat ')
+                                          ->setParameter(':vulnerability', $context['vulnerability'])
+                                          ->setParameter(':threat', $context['threat'])
+                                          ->setParameter(':asset', $context['asset']);
+                                      if (empty($context['anr'])) {
+                                          $res = $res->andWhere(' a.anr IS NULL ');
+                                      } else {
+                                          $res = $res->andWhere(' a.anr = :anr ')
+                                              ->setParameter(':anr', $context['anr']);
+                                      }
+                                      $res = $res->getQuery()
+                                          ->getResult();
+                                      $context['id'] = empty($context['id']) ? $this->get('id') : $context['id'];
+                                      if (!empty($res) && $context['id'] != $res[0]['id']) {
+                                          return false;
+                                      }
+                                  }
+                                  return true;
+                              } else {
+                                  return true;
+                              }
+                          },
+                      ),
+                  ),
+              ),
+          ));
+      }
+      return $this->inputFilter;
+  }
 }
