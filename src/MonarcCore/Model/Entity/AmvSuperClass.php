7490adc3c01beecef9e3b5192af26f548400a1d6
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-05-03 15:28:28 +0200

    Add callback to test the unicity of an AMV

--- src/MonarcCore/Model/Entity/AmvSuperClass.php
+++ src/MonarcCore/Model/Entity/AmvSuperClass.php
@@ -301,4 +301,100 @@ class AmvSuperclass extends AbstractEntity
         ];
         return [$filterJoin, $filterLeft, $filtersCol];
     }
+
+    public function getInputFilter($partial = false)
+    {
+        if (!$this->inputFilter) {
+            parent::getInputFilter($partial);
+
+            $texts = ['vulnerability', 'asset'];
+
+            foreach ($texts as $text) {
+                $this->inputFilter->add(array(
+                    'name' => $text,
+                    'required' => ($partial) ? false : true,
+                    'allow_empty' => false,
+                    // 'filters' => array(
+                    //     array(
+                    //         'name' => 'Digits',
+                    //     ),
+                    // ),
+                    'validators' => array(),
+                ));
+            }
+
+            $this->inputFilter->add(array(
+                'name' => 'threat',
+                'required' => ($partial) ? false : true,
+                'allow_empty' => false,
+                // 'filters' => array(
+                //     array(
+                //         'name' => 'Digits',
+                //     ),
+                // ),
+                'validators' => array(
+                    array(
+                        'name' => 'Callback',//'\MonarcCore\Validator\UniqueAMV',
+                        'options' => array(
+                            'messages' => array(
+                                \Zend\Validator\Callback::INVALID_VALUE => 'This AMV link is already used',
+                            ),
+                            'callback' => function ($value, $context = array()) use ($partial) {
+                                if (!$partial) {
+                                    $adapter = $this->getDbAdapter();
+                                    if (empty($adapter)) {
+                                        return false;
+                                    } else { //BO case
+                                        $res = $adapter->getRepository(get_class($this))->createQueryBuilder('a')
+                                            ->select(array('a.uuid'));
+                                        if (empty($context['anr'])) {
+                                          $res->innerJoin('a.vulnerability','vulnerability')
+                                              ->innerJoin('a.threat','threat')
+                                              ->innerJoin('a.asset','asset')
+                                              ->where('vulnerability.uuid = :vulnerability')
+                                              ->andWhere('asset.uuid = :asset')
+                                              ->andWhere('threat.uuid = :threat')
+                                              ->andWhere('vulnerability.anr IS NULL')
+                                              ->andWhere('asset.anr IS NULL')
+                                              ->andWhere('threat.anr IS NULL ')
+                                              ->andWhere(' a.anr  IS NULL')
+                                              ->setParameter(':vulnerability', $context['vulnerability'])
+                                              ->setParameter(':threat', $context['threat'])
+                                              ->setParameter(':asset', $context['asset']);
+                                        } else { //FO case
+                                          file_put_contents('php://stderr', print_r($context, TRUE).PHP_EOL);
+                                           $res ->innerJoin('a.vulnerability','vulnerability')
+                                                ->innerJoin('a.threat','threat')
+                                                ->innerJoin('a.asset','asset')
+                                                ->where('vulnerability.uuid = :vulnerability ')
+                                                ->andWhere('asset.uuid = :asset ')
+                                                ->andWhere('threat.uuid = :threat ')
+                                                ->andWhere('vulnerability.anr = :anr ')
+                                                ->andWhere('asset.anr = :anr ')
+                                                ->andWhere('threat.anr = :anr ')
+                                                ->andWhere(' a.anr = :anr ')
+                                                ->setParameter(':vulnerability', $context['vulnerability']['uuid'])
+                                                ->setParameter(':threat', $context['threat']['uuid'])
+                                                ->setParameter(':asset', $context['asset']['uuid'])
+                                                ->setParameter(':anr', $context['anr']);
+                                        }
+                                        $res = $res->getQuery()
+                                            ->getResult();
+                                        $context['uuid'] = empty($context['uuid']) ? $this->get('uuid'): $context['uuid'];
+                                        if (!empty($res) && $context['uuid'] != $res[0]['uuid']) {
+                                            return false;
+                                        }
+                                    }
+                                    return true;
+                                } else {
+                                    return true;
+                                }
+                            },
+                        ),
+                    ),
+                ),
+            ));
+        }
+        return $this->inputFilter;
+    }
 }
