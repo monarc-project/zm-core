7b6b8dc7bd7c136bd0de49ac29619453df623bc6
Jerome Lombardi <jerome.lombardi@cases.lu>
2019-08-07 15:07:19 +0200

    adapt the scripts to mysql

--- migrations/db/20190806160623_fix_date_value.php
+++ migrations/db/20190806160623_fix_date_value.php
@@ -27,19 +27,22 @@ class FixDateValue extends AbstractMigration
      */
     public function change()
     {
-        $this->execute("UPDATE rolf_risks SET created_at='1970-01-01 00:00:00' WHERE created_at='0000-00-00 00:00:00';");
-        $this->execute("UPDATE rolf_risks SET updated_at='1970-01-01 00:00:00' WHERE updated_at='0000-00-00 00:00:00';");
+        $this->execute("UPDATE rolf_risks SET created_at = DATE('1970-01-01')  WHERE CAST(created_at AS CHAR(20)) = '0000-00-00 00:00:00' or created_at is null;");
+        $this->execute("UPDATE rolf_risks SET updated_at = DATE('1970-01-01')  WHERE CAST(updated_at AS CHAR(20)) = '0000-00-00 00:00:00' or updated_at is null;");
 
-        $this->execute("UPDATE rolf_tags SET created_at='1970-01-01 00:00:00' WHERE created_at='0000-00-00 00:00:00';");
-        $this->execute("UPDATE rolf_tags SET updated_at='1970-01-01 00:00:00' WHERE updated_at='0000-00-00 00:00:00';");
+        $this->execute("UPDATE rolf_tags SET created_at = DATE('1970-01-01')  WHERE CAST(created_at AS CHAR(20)) = '0000-00-00 00:00:00' or created_at is null;");
+        $this->execute("UPDATE rolf_tags SET updated_at = DATE('1970-01-01')  WHERE CAST(updated_at AS CHAR(20)) = '0000-00-00 00:00:00' or updated_at is null;");
 
-        $this->execute("UPDATE objects SET created_at='1970-01-01 00:00:00' WHERE created_at='0000-00-00 00:00:00';");
-        $this->execute("UPDATE objects SET updated_at='1970-01-01 00:00:00' WHERE updated_at='0000-00-00 00:00:00';");
+        $this->execute("UPDATE rolf_risks_tags  SET created_at = DATE('1970-01-01')  WHERE CAST(created_at AS CHAR(20)) = '0000-00-00 00:00:00' or created_at is null;");
+        $this->execute("UPDATE rolf_risks_tags  SET updated_at = DATE('1970-01-01')  WHERE CAST(updated_at AS CHAR(20)) = '0000-00-00 00:00:00' or updated_at is null;");
 
-        $this->execute("UPDATE guides SET created_at='1970-01-01 00:00:00' WHERE created_at is NULL;");
-        $this->execute("UPDATE guides SET updated_at='1970-01-01 00:00:00' WHERE updated_at is NULL;");
+        $this->execute("UPDATE objects SET created_at = DATE('1970-01-01')  WHERE CAST(created_at AS CHAR(20)) = '0000-00-00 00:00:00' or created_at is null;");
+        $this->execute("UPDATE objects SET updated_at = DATE('1970-01-01')  WHERE CAST(updated_at AS CHAR(20)) = '0000-00-00 00:00:00' or updated_at is null;");
 
-        $this->execute("UPDATE objects_categories SET created_at='1970-01-01 00:00:00' WHERE created_at is NULL;");
-        $this->execute("UPDATE objects_categories SET updated_at='1970-01-01 00:00:00' WHERE updated_at is NULL;");
+        $this->execute("UPDATE guides SET created_at = DATE('1970-01-01')  WHERE CAST(created_at AS CHAR(20)) = '0000-00-00 00:00:00' or created_at is null;");
+        $this->execute("UPDATE guides SET updated_at = DATE('1970-01-01')  WHERE CAST(updated_at AS CHAR(20)) = '0000-00-00 00:00:00' or updated_at is null;");
+
+        $this->execute("UPDATE objects_categories SET created_at = DATE('1970-01-01')  WHERE CAST(created_at AS CHAR(20)) = '0000-00-00 00:00:00' or created_at is null;");
+        $this->execute("UPDATE objects_categories SET updated_at = DATE('1970-01-01')  WHERE CAST(updated_at AS CHAR(20)) = '0000-00-00 00:00:00' or updated_at is null;");
     }
 }
