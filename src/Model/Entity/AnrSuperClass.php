1d13715cac5b30fb7b7cae2460d9d5f268257d2b
Ruslan Baidan <ruslan.baidan@cases.lu>
2019-10-17 10:03:45 +0200

    Code styles fixes.

--- src/Model/Entity/AnrSuperClass.php
+++ src/Model/Entity/AnrSuperClass.php
@@ -23,7 +23,7 @@ class AnrSuperClass extends AbstractEntity
     use Traits\UpdateEntityTrait;
 
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
@@ -67,140 +67,140 @@ class AnrSuperClass extends AbstractEntity
     protected $label4;
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="description1", type="text", length=255, nullable=true)
      */
     protected $description1;
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="description2", type="text", length=255, nullable=true)
      */
     protected $description2;
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="description3", type="text", length=255, nullable=true)
      */
     protected $description3;
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="description4", type="text", length=255, nullable=true)
      */
     protected $description4;
 
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="seuil1", type="integer", options={"unsigned":true, "default":4})
      */
     protected $seuil1 = 4;
 
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="seuil2", type="integer", options={"unsigned":true, "default":8})
      */
     protected $seuil2 = 8;
 
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="seuil_rolf1", type="integer", options={"unsigned":true, "default":4})
      */
     protected $seuilRolf1 = 4;
 
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="seuil_rolf2", type="integer", options={"unsigned":true, "default":8})
      */
     protected $seuilRolf2 = 8;
 
     /**
-     * @var integer
+     * @var int
      *
      * @ORM\Column(name="seuil_traitement", type="integer", options={"unsigned":true})
      */
     protected $seuilTraitement;
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="init_anr_context", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $initAnrContext = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="init_eval_context", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $initEvalContext = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="init_risk_context", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $initRiskContext = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="init_def_context", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $initDefContext = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="init_livrable_done", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $initLivrableDone = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="model_summary", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $modelSummary = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="model_livrable_done", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $modelLivrableDone = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="eval_risks", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $evalRisks = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="eval_plan_risks", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $evalPlanRisks = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="eval_livrable_done", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $evalLivrableDone = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="manage_risks", type="smallint", options={"unsigned":true, "default":0})
      */
@@ -208,62 +208,56 @@ class AnrSuperClass extends AbstractEntity
 
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="context_ana_risk", type="text", length=255, nullable=true)
      */
     protected $contextAnaRisk;
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="context_gest_risk", type="text", length=255, nullable=true)
      */
     protected $contextGestRisk;
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="synth_threat", type="text", length=255, nullable=true)
      */
     protected $synthThreat;
 
     /**
-     * @var text
+     * @var string
      *
      * @ORM\Column(name="synth_act", type="text", length=255, nullable=true)
      */
     protected $synthAct;
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="cache_model_show_rolf_brut", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $cacheModelShowRolfBrut = '0';
 
     /**
-     * @var smallint
+     * @var int
      *
      * @ORM\Column(name="show_rolf_brut", type="smallint", options={"unsigned":true, "default":0})
      */
     protected $showRolfBrut = '0';
 
-    /**
-     * @return int
-     */
-    public function getId()
+    public function getId(): int
     {
         return $this->id;
     }
 
-    /**
-     * @param int $id
-     * @return Model
-     */
-    public function setId($id)
+    public function setId(int $id): self
     {
         $this->id = $id;
+
         return $this;
     }
 
@@ -279,9 +273,10 @@ class AnrSuperClass extends AbstractEntity
      * @param Object $objects
      * @return Anr
      */
-    public function setObjects($objects)
+    public function setObjects($objects): self
     {
         $this->objects = $objects;
+
         return $this;
     }
 }
