cd93bc4654bc8af7120ac5684e145b9659ad84a4
Cédric Bonhomme <cedric@cedricbonhomme.org>
2020-01-03 09:54:23 +0100

    Updated copyright years.

--- src/Model/Entity/SoaCategorySuperClass.php
+++ src/Model/Entity/SoaCategorySuperClass.php
@@ -1,7 +1,7 @@
 <?php
 /**
 * @link      https://github.com/monarc-project for the canonical source repository
-* @copyright Copyright (c) 2016-2019  SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
+* @copyright Copyright (c) 2016-2020 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
 * @license   MONARC is licensed under GNU Affero General Public License version 3
 */
 
