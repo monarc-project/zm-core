922c4b01b73979aab46dd62619e59486252757c9
Ruslan Baidan <ruslan.baidan@cases.lu>
2020-01-20 16:07:18 +0100

    FIxed the notice of the extra property in the list assigment.

--- src/Service/AssetExportService.php
+++ src/Service/AssetExportService.php
@@ -7,9 +7,12 @@
 
 namespace Monarc\Core\Service;
 
+use Doctrine\ORM\Mapping\MappingException;
 use Doctrine\ORM\Query\QueryException;
-use Doctrine\ORM\Query\MappingException;
 use Doctrine\DBAL\Exception\DriverException;
+use Monarc\Core\Exception\Exception;
+use Monarc\Core\Model\Entity\AmvSuperClass;
+use Monarc\Core\Model\Entity\AnrSuperClass;
 
 /**
  * Asset Service Export
@@ -23,28 +26,28 @@ class AssetExportService extends AbstractService
 
     /**
      * Generates the array to be exported into a file
+     *
      * @param int $id The asset ID
+     * @param AnrSuperClass $anr
      * @param string $filename The filename to put into
+     *
      * @return array The exported data
-     * @throws \Monarc\Core\Exception\Exception
+     * @throws Exception
      */
-    public function generateExportArray($id, $anr = null,&$filename = "")
+    public function generateExportArray($id, $anr = null, &$filename = '')
     {
         if (empty($id)) {
-            throw new \Monarc\Core\Exception\Exception('Asset to export is required', 412);
+            throw new Exception('Asset to export is required', 412);
         }
 
-        try{
-          $entity = $this->get('table')->getEntity(['uuid' => $id, "anr" => $anr]);
-        }
-        catch(MappingException $e){
-          $entity = $this->get('table')->getEntity($id);
-        } catch (QueryException $e) {
-             $entity = $this->get('table')->getEntity($id);
+        try {
+            $entity = $this->get('table')->getEntity(['uuid' => $id, 'anr' => $anr]);
+        } catch (MappingException | QueryException $e) {
+            $entity = $this->get('table')->getEntity($id);
         }
 
         if (empty($entity)) {
-            throw new \Monarc\Core\Exception\Exception('Asset not found', 412);
+            throw new Exception('Asset not found', 412);
         }
 
         $filename = preg_replace("/[^a-z0-9\._-]+/i", '', $entity->get('code'));
@@ -73,50 +76,23 @@ class AssetExportService extends AbstractService
         $amvService = $this->get('amvService');
         $amvTable = $amvService->get('table');
         $anrId = $entity->get('anr');
-        try{
-          $amvResults = $amvTable->getEntityByFields(['asset' => ['uuid' => $entity->getUuid()->toString(), 'anr' => $anrId]]);
-        }catch(QueryException $e){
-          $amvResults = $amvTable->getEntityByFields(['asset' => $entity->getUuid()->toString(), 'anr' => $anrId]);
-      } catch(MappingException $e) {
-          $amvResults = $amvTable->getEntityByFields(['asset' => $entity->getUuid()->toString(), 'anr' => $anrId]);
-      }catch(DriverException $e) {
-          $amvResults = $amvTable->getEntityByFields(['asset' => $entity->getUuid()->toString(), 'anr' => $anrId]);
-      }
+        try {
+            $amvResults = $amvTable->getEntityByFields(['asset' => ['uuid' => (string)$entity->getUuid(), 'anr' => $anrId]]);
+        } catch (QueryException | MappingException | DriverException $e) {
+            $amvResults = $amvTable->getEntityByFields(['asset' => (string)$entity->getUuid(), 'anr' => $anrId]);
+        }
 
-
+        /** @var AmvSuperClass $amv */
         foreach ($amvResults as $amv) {
-            list(
-                $return['amvs'][$amv->get('uuid')->toString()],
+            list($return['amvs'][(string)$amv->getUuid()],
                 $threats,
-                $vulns,
+                $vulnerabilities,
                 $themes,
-                $measures,
-                $soacategories) = $amvService->generateExportArray($amv,$anrId);
-            if (empty($return['threats'])) {
-                $return['threats'] = $threats;
-            } else {
-                $return['threats'] += $threats;
-            }
-            if (empty($return['themes'])) {
-                $return['themes'] = $themes;
-            } else {
-                $return['themes'] += $themes;
-            }
-            if (empty($return['vuls'])) {
-                $return['vuls'] = $vulns;
-            } else {
-                $return['vuls'] += $vulns;
-            }
-            if (empty($return['measures'])) {
-                $return['measures'] = $measures;
-            } else {
-                $return['measures'] += $measures;
-            }
-            if (empty($return['soacategories'])) {
-                $return['soacategories'] = $soacategories;
-            } else {
-                $return['soacategories'] += $soacategories;
-            }
+                $measures) = $amvService->generateExportArray($amv, $anrId);
+            $return['threats'][] = $threats;
+            $return['themes'][] = $themes;
+            $return['vuls'][] = $vulnerabilities;
+            $return['measures'][] = $measures;
         }
 
         return $return;
