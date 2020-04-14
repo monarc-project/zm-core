cbfcabf875a8ded8d1d1b6d2fa9e900023fc433f
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2020-04-14 11:55:19 +0200

    Fixed the return type.

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -923,7 +923,7 @@ abstract class AbstractService extends AbstractServiceFactory
         }
     }
 
-    protected function getConnectedUser(): UserSuperClass
+    protected function getConnectedUser(): ?UserSuperClass
     {
         return $this->get('table')->getConnectedUser();
     }
