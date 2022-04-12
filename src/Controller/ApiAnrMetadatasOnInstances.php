bfc758621eb4b978fd97871ea2ae3050cd1071e2
jerolomb <jerome.lombardi@cases.lu>
2022-04-12 11:10:40 +0200

    fix typo

--- src/Controller/ApiAnrMetadatasOnInstances.php
+++ src/Controller/ApiAnrMetadatasOnInstances.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link      https://github.com/monarc-project for the canonical source repository
- * @copyright Copyright (c) 2016-2020 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
+ * @copyright Copyright (c) 2016-2022 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
  * @license   MONARC is licensed under GNU Affero General Public License version 3
  */
 
@@ -10,9 +10,9 @@ namespace Monarc\Core\Controller;
 use Laminas\View\Model\JsonModel;
 
 /**
- * Api Anr Scales Comments Controller
+ * Api Anr Metadatas On Instances Controller
  *
- * Class ApiAnrScalesCommentsController
+ * Class ApiAnrMetadatasOnInstancesController
  * @package Monarc\Core\Controller
  */
 class ApiAnrMetadatasOnInstancesController extends AbstractController
