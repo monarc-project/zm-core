726582b2f9ba300d2e40837fbf8d1ac12725641b
jerolomb <jerome.lombardi@cases.lu>
2022-04-12 09:27:25 +0200

    first draft for AnrMetadatasOnInstances entity/table

---
+++ src/Model/Entity/AnrMetadatasOnInstancesSuperClass.php
@@ -0,0 +1,83 @@
+<?php declare(strict_types=1);
+/**
+ * @link      https://github.com/monarc-project for the canonical source repository
+ * @copyright Copyright (c) 2016-2022 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
+ * @license   MONARC is licensed under GNU Affero General Public License version 3
+ */
+
+namespace Monarc\Core\Model\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Monarc\Core\Model\Entity\Traits\CreateEntityTrait;
+use Monarc\Core\Model\Entity\Traits\UpdateEntityTrait;
+
+/**
+ * @ORM\Table(name="anr_metadatas_on_instances", indexes={
+ *      @ORM\Index(name="anr_id", columns={"anr_id"})
+ * })
+ * @ORM\MappedSuperclass
+ * @ORM\HasLifecycleCallbacks()
+ */
+class AnrMetadatasOnInstancesSuperClass
+{
+    use CreateEntityTrait;
+    use UpdateEntityTrait;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var AnrSuperClass
+     *
+     * @ORM\ManyToOne(targetEntity="Anr", cascade={"persist"})
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="anr_id", referencedColumnName="id", nullable=true, onDelete="CASCADE")
+     * })
+     */
+    protected $anr;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="label_translation_key", type="string", length=255, options={"default": ""})
+     */
+    protected $labelTranslationKey = '';
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getAnr(): AnrSuperClass
+    {
+        return $this->anr;
+    }
+
+    public function setAnr(AnrSuperClass $anr): self
+    {
+        $this->anr = $anr;
+
+        return $this;
+    }
+
+    public function getLabelTranslationKey(): string
+    {
+        return $this->labelTranslationKey;
+    }
+
+    public function setLabelTranslationKey(string $labelTranslationKey): self
+    {
+        $this->labelTranslationKey = $labelTranslationKey;
+
+        return $this;
+    }
+}
