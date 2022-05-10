9af9ef9c1caf08ac6f90d09c9ede82f2693beb3e
jerolomb <jerome.lombardi@cases.lu>
2022-05-10 09:16:02 +0200

    first draft for tables and entities, and fix migration script

--- migrations/db/20220510110500_add_customizable_soa_scale.php
+++ migrations/db/20220510110500_add_customizable_soa_scale.php
@@ -23,21 +23,24 @@ class AddCustomizableSoaScale extends AbstractMigration
         $table->changeColumn('id', 'integer', array('identity'=>true, 'signed'=>false))->update();
         $table->addForeignKey('anr_id', 'anrs', 'id', array('delete' => 'CASCADE','update' => 'RESTRICT'))->update();
 
-        //create the comments, one comment by level
+        //create the comments, one comment by scale_index
         $table = $this->table('soa_scale_comments');
         $table
+            ->addColumn('anr_id', 'integer', array('null' => true, 'signed' => false))
             ->addColumn('soa_scale_id', 'integer', array('null' => true, 'signed' => false))
             ->addColumn('scale_index', 'integer', array('null' => true, 'signed' => false))
-            ->addColumn('colour', 'string', array('null' => true, 'limit' => 255))
-            ->addColumn('comment_translation_key', 'string', array('null' => true, 'limit' => 7))
+            ->addColumn('colour', 'string', array('null' => true, 'limit' => 7))
+            ->addColumn('comment_translation_key', 'string', array('null' => true, 'limit' => 255))
+            ->addColumn('is_hidden', 'boolean', array('default' => false, 'null' => false))
             ->addColumn('creator', 'string', array('null' => true, 'limit' => 255))
             ->addColumn('created_at', 'datetime', array('null' => true))
             ->addColumn('updater', 'string', array('null' => true, 'limit' => 255))
             ->addColumn('updated_at', 'datetime', array('null' => true))
-            ->addIndex(array('soa_scale_id'))
+            ->addIndex(array('soa_scale_id','anr_id'))
             ->create();
         $table->changeColumn('id', 'integer', array('identity'=>true, 'signed'=>false))->update();
         $table->addForeignKey('soa_scale_id', 'soa_scale', 'id', array('delete' => 'CASCADE','update' => 'RESTRICT'))
+            ->addForeignKey('anr_id', 'anrs', 'id', array('delete' => 'CASCADE','update' => 'RESTRICT'))
             ->update();
     }
 }
---
+++ src/Model/Table/SoaScaleTable.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link      https://github.com/monarc-project for the canonical source repository
+ * @copyright Copyright (c) 2016-2022 SMILE GIE Securitymadein.lu - Licensed under GNU Affero GPL v3
+ * @license   MONARC is licensed under GNU Affero General Public License version 3
+ */
+
+namespace Monarc\Core\Model\Table;
+
+use Monarc\Core\Model\Db;
+use Monarc\Core\Model\Entity\SoaScale;
+use Monarc\Core\Service\ConnectedUserService;
+
+/**
+ * Class SoaScaleTable
+ * @package Monarc\Core\Model\Table
+ */
+class SoaScaleTable extends AbstractEntityTable
+{
+    public function __construct(Db $dbService, ConnectedUserService $connectedUserService)
+    {
+        parent::__construct($dbService, SoaCategory::class, $connectedUserService);
+    }
+}
