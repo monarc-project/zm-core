78905aaadc42895f7f29ca13366257ff6fc663ff
Cédric Bonhomme <cedric@cedricbonhomme.org>
2022-11-09 14:38:22 +0100

    chg: [Adapter] Simplify authentication function.

--- src/Adapter/Authentication.php
+++ src/Adapter/Authentication.php
@@ -26,8 +26,8 @@ class Authentication extends AbstractAdapter
     /** @var ConfigService */
     private $configService;
 
-    const TWO_FA_AUTHENTICATION_REQUIRED = 2;
-    const TWO_FA_AUTHENTICATION_TO_SET_UP = 3;
+    const TWO_FA_REQUIRED = 2;
+    const TWO_FA_TO_SET_UP = 3;
 
     public function __construct(UserTable $userTable, ConfigService $configService)
     {
@@ -68,53 +68,55 @@ class Authentication extends AbstractAdapter
             return new Result(Result::FAILURE_IDENTITY_NOT_FOUND, $this->getIdentity());
         }
 
-        // TODO: make the test on dateStart and dateEnd
         if ($user->isActive()) {
             if (password_verify($credential, $user->getPassword())) {
                 $this->setUser($user);
-                // if the user is using Two Factor Authentication
-                if ($user->isTwoFactorAuthEnabled()) {
-                    // check if the 2FA token has been submitted
-                    if (empty($token)) {
-                        return new Result(self::TWO_FA_AUTHENTICATION_REQUIRED, $this->getIdentity());
-                    } else {
+
+                /* Validate if the user has 2FA enabled. */
+                if (!$user->isTwoFactorAuthEnabled()) {
+                    /* Validate if 2FA is enforced on the platform. */
+                    if ($this->configService->isTwoFactorAuthEnforced() && empty($token)) {
+                        /* 2FA enforced and missing tokens in order to activate 2FA */
+                        return new Result(self::TWO_FA_TO_SET_UP, $this->getIdentity());
+                    } elseif ($this->configService->isTwoFactorAuthEnforced()) {
+                        /* 2FA enforced and received tokens in order to activate 2FA */
+                        /* tokens are the verification code and the otp secret */
+                        $tokens = explode(":", $token);
                         // verify the submitted OTP token
                         $tfa = new TwoFactorAuth('MONARC TwoFactorAuth');
-                        if ($tfa->verifyCode($user->getSecretKey(), $token)) {
+                        if ($tfa->verifyCode($tokens[0], $tokens[1])) {
+                            $user->setSecretKey($tokens[0]);
+                            $user->setTwoFactorAuthEnabled(true);
+                            $this->userTable->saveEntity($user);
                             return new Result(Result::SUCCESS, $this->getIdentity());
                         }
-
-                        // verify the submitted recovery code
-                        $recoveryCodes = $user->getRecoveryCodes();
-                        foreach ($recoveryCodes as $key => $recoveryCode) {
-                            if (password_verify($token, $recoveryCode)) {
-                                unset($recoveryCodes[$key]);
-                                $user->setRecoveryCodes($recoveryCodes);
-                                $this->userTable->saveEntity($user);
-
-                                return new Result(Result::SUCCESS, $this->getIdentity());
-                            }
-                        }
+                        return new Result(self::TWO_FA_TO_SET_UP, $this->getIdentity());
                     }
-                } elseif ($this->configService->isTwoFactorAuthEnforced()) {
-                    if (empty($token)) {
-                        // if two factor authentication is enforced and the user has not yet enabled it
-                        return new Result(self::TWO_FA_AUTHENTICATION_TO_SET_UP, $this->getIdentity());
-                    }
-                    $tokens = explode(":", $token); # verificationCode and otpSecret tokens
-                    // verify the submitted OTP token
-                    $tfa = new TwoFactorAuth('MONARC TwoFactorAuth');
-                    if ($tfa->verifyCode($tokens[0], $tokens[1])) {
-                        $user->setSecretKey($tokens[0]);
-                        $user->setTwoFactorAuthEnabled(true);
-                        $this->userTable->saveEntity($user);
-                        return new Result(Result::SUCCESS, $this->getIdentity());
-                    }
-                } else {
                     return new Result(Result::SUCCESS, $this->getIdentity());
                 }
 
-                return new Result(Result::FAILURE_CREDENTIAL_INVALID, $this->getIdentity());
+                /* Validate if the 2FA token has been submitted. */
+                if (empty($token)) {
+                    return new Result(static::TWO_FA_REQUIRED, $this->getIdentity());
+                }
+
+                /* Verify the submitted OTP token. */
+                $tfa = new TwoFactorAuth('MONARC TwoFactorAuth');
+                if ($tfa->verifyCode($user->getSecretKey(), $token)) {
+                    return new Result(Result::SUCCESS, $this->getIdentity());
+                }
+
+                /* Verify the submitted recovery code. */
+                $recoveryCodes = $user->getRecoveryCodes();
+                foreach ($recoveryCodes as $key => $recoveryCode) {
+                    if (password_verify($token, $recoveryCode)) {
+                        unset($recoveryCodes[$key]);
+                        $user->setRecoveryCodes($recoveryCodes);
+                        $this->userTable->saveEntity($user);
+
+                        return new Result(Result::SUCCESS, $this->getIdentity());
+                    }
+                }
             }
 
             return new Result(Result::FAILURE_CREDENTIAL_INVALID, $this->getIdentity());
