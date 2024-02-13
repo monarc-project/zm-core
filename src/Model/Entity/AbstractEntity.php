f10274c6e2a2d21dc8f2766bb8c6c8808f56a534
Ruslan Baidan <ruslan.baidan@nc3.lu>
2024-02-13 14:14:12 +0100

    Update AbstractEntity.php
    Fixed the validation of the getUuid existence in the exchangeArray method.

--- src/Model/Entity/AbstractEntity.php
+++ src/Model/Entity/AbstractEntity.php
@@ -225,8 +225,8 @@ abstract class AbstractEntity implements InputFilterAwareInterface
         if (!$this->squeezeAutoPositionning && isset($this->parameters['implicitPosition']['field'])) {
             $parent_before = $this->get($this->parameters['implicitPosition']['field']);
             if (is_object($parent_before)) {
-                $parent_before = !$parent_before instanceof AnrSuperClass && $parent_before->getUuid() !== null
-                    ? $parent_before->getUuid()
+                $parent_before = !$parent_before instanceof AnrSuperClass && $parent_before->get('uuid') !== null
+                    ? $parent_before->get('uuid')
                     : $parent_before->get('id');
             }
             $parent_after = array_key_exists($this->parameters['implicitPosition']['field'], $options) ? $options[$this->parameters['implicitPosition']['field']] : null;
