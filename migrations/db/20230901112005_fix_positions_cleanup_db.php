ea4cc43b5c9246945a5b01667894d09ef232bef6
Ruslan Baidan <ruslan.baidan@lhc.lu>
2024-10-06 22:06:47 +0200

    FIxed the columns order in the migration.

--- migrations/db/20230901112005_fix_positions_cleanup_db.php
+++ migrations/db/20230901112005_fix_positions_cleanup_db.php
@@ -182,7 +182,7 @@ class FixPositionsCleanupDb extends AbstractMigration
         /* Correct MeasuresMeasures table structure. */
         $this->table('measures_measures')
             ->addColumn('id', 'integer', ['signed' => false, 'after' => MysqlAdapter::FIRST])
-            ->dropForeignKey(['father_id', 'child_id'])
+            ->dropForeignKey(['child_id', 'father_id'])
             ->renameColumn('father_id', 'master_measure_id')
             ->renameColumn('child_id', 'linked_measure_id')
             ->removeColumn('creator')
